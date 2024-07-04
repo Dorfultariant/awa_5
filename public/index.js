cde7d95204d6f5a44fab47c11ff7d23f6a2068fc
TH <tunxero@hotmail.com>
2024-07-04 11:11:17 +0300

    typo to search-field

--- public/index.js
+++ public/index.js
@@ -18,7 +18,7 @@ document.addEventListener("DOMContentLoaded", async () => {
 
     const submitBtn = document.getElementById("submit");
 
-    const searchField = document.getElementById("seach-field");
+    const searchField = document.getElementById("seacrh-field");
 
     // Add ingredients to a list
     addIngredientBtn.addEventListener("click", async () => {
